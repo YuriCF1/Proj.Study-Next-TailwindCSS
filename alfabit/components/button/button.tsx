70769f84941ff59e330742f9a833d7b2b9d3cd0e
Yuri Cruz <yuricruzf@gmail.com>
2023-12-23 23:50:40 -0300

    Creating button component

---
+++ alfabit/components/button/button.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+type ButtonProps = {
+    children: React.ReactNode
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+// const button = (props: ButtonProps) => {
+const button = ({ children, className }: ButtonProps) => {
+    return (
+        // <button>{props.children}</button>
+        <button
+            className={`
+            bg-[#2D5BFF]
+            rounded-[8px] 
+            px-[32px]
+            py-[12px]
+            minhas-estilizacoes
+        ${className}`}>
+            {children}
+        </button>
+    )
+}
+
+export default button
