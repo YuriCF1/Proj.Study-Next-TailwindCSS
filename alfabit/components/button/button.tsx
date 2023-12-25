76ea5d2558b78092e92cafb74c59ee1d0cb181f7
Yuri Cruz <yuricruzf@gmail.com>
2023-12-25 19:14:48 -0300

    Setting more examples

--- alfabit/components/button/button.tsx
+++ alfabit/components/button/button.tsx
@@ -5,20 +5,29 @@ type ButtonProps = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 // const button = (props: ButtonProps) => {
+// Colocando o className para que algúem possa injetar qualquer classe
 const button = ({ children, className }: ButtonProps) => {
     return (
         // <button>{props.children}</button>
-        <button
-            className={`
+        <>
+            <button
+                className={`
+            minhas-estilizacoes
             bg-[#2D5BFF]
             rounded-[8px] 
             px-[32px]
             py-[12px]
-            minhas-estilizacoes
             text-white
-        ${className}`}>
-            {children}
-        </button>
+            ${className}`}>
+                {children}
+            </button>
+            {/* Outros exemplos importantes */}
+            {/* <div className="bg-[#2D5BFF] rounded-t-[10px]">Borda superior arredondada com 10 pixels</div>
+            <div className="bg-[#2D5BFF] rounded-r-[10px]">Borda direita arredondada com 10 pixels</div>
+            <div className="bg-[#2D5BFF] rounded-b-[10px]">Borda inferior arredondada com 10 pixels</div>
+            <div className="bg-[#2D5BFF] rounded-l-[10px]">Borda esquerda arredondada com 10 pixels</div>
+            <div className="bg-[#2D5BFF] rounded-br-[10px] m-5 px-4 py-2">Borda inferior direita arredondada com 10 pixels</div> */}
+        </>
     )
 }
 
