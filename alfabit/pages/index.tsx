10d21c701e148a169c4ae350ef938463a3debdd6
Yuri Cruz <yuricruzf@gmail.com>
2023-12-26 23:16:45 -0300

    Importing coimponent

--- alfabit/pages/index.tsx
+++ alfabit/pages/index.tsx
@@ -1,8 +1,11 @@
+import Button from "@/components/button/button"
+
 const App = () => {
   return (
     <>
       {/* Exemplo de utilização das variáveis */}
       {/* <h1 className="text-example bg-example2">Hello design system</h1> */}
+      <Button>Botão</Button>
     </>
   )
 }
