33d81a13dd12f3f69f5c83b7bf8630bf3654bad7
Yuri Cruz <yuricruzf@gmail.com>
2024-01-03 14:01:58 -0300

    Setting new class with function to define opacity

--- alfabit/tailwind.config.js
+++ alfabit/tailwind.config.js
@@ -1,19 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: { //Tudo dentro de theme, sobrescreve. Exemplo:^Vai sobrescrever as cores do tailwind
-    },
+    // colors: { //Tudo dentro de theme, sobrescreve. Exemplo:^Vai sobrescrever as cores do tailwind
+    // },
     extend: {
-      colors: { //O que estiver no extend, vai usar as cores padrão e vai extender, adicionando novas
+      colors: {
+        //O que estiver no extend, vai usar as cores padrão e vai extender, adicionando novas
         // example: "#f58912"
-        primary: 'var(--primaryColor)',
-        example: 'var(--example)',
-        example2: 'var(--example2)'
+        primary: "var(--primaryColor)",
+        primaryOpacity: ({ opacityValue }) =>
+          `rgba(var(--primaryColorOpacity), ${opacityValue})`,
+
+        // Pode-se fazer contas matemáticas também
+        /*colors: {
+            prmiary: ({ opacityValue }) => `rgba(var(--primary), ${opacityValue/2})`
+        }*/
+        // _____ou______
+        /*
+          colors: ({ theme }) => {
+            primary: theme('var(--primary) / 75%')
+          }
+        */
+
+        example: "var(--example)",
+        example2: "var(--example2)",
       },
       // backgroundImage: {
       //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -23,4 +38,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
