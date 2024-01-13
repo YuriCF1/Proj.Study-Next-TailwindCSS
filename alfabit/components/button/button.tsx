4b514ea57c2bcc0d05b0660065cf98ef055301e5
Yuri Cruz <yuricruzf@gmail.com>
2024-01-12 22:09:39 -0300

    Refactoring butotn, turning into function first

--- alfabit/components/button/button.tsx
+++ alfabit/components/button/button.tsx
@@ -6,47 +6,59 @@ export type ButtonProps = {
 
 // const button = (props: ButtonProps) => {
 // Colocando o className para que algúem possa injetar qualquer classe
-const Button = ({ children, className }: ButtonProps) => {
-    return (
-        // <button>{props.children}</button>
+// ...rest = resto das propriedades
+const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
+    const generalStyles = 'rounded-md px-6 py-2'
+    const Btn = (classes: string) => {
+        return <button
+            className={`${generalStyles} ${classes} ${className}`}
+            disabled={disabled}
+            {...rest}>
+            {children}
+        </button>
+    }
+    return Btn(disabled ? 'bg-bg-disabled text-text-disabled' : 'bg-primary text-white')
+    // <button>{props.children}</button>
 
-        // MANEIRAS DE DECLARAR AS CLASSES
-        // bg-[#2D5BFF] = Valores arbitrários. Valores que não estão definidos como tokens/configurações padrão do tailwind
-        // bg-[#2D5BFF] = bg-primary
-        // theme-violet
+    // MANEIRAS DE DECLARAR AS CLASSES
+    // bg-[#2D5BFF] = Valores arbitrários. Valores que não estão definidos como tokens/configurações padrão do tailwind
+    // bg-[#2D5BFF] = bg-primary
+    // theme-violet
 
-        // Quando se tem a cor padrão, a opacidade é aplicada
-        // bg-blue-500
-        // bg-opacity-50
+    // Quando se tem a cor padrão, a opacidade é aplicada
+    // bg-blue-500
+    // bg-opacity-50
 
-        // Tem várias formas de consertar.
-        //Verificar o tailwind.config e ver que dá pra transformar a classe em função
+    // Tem várias formas de consertar.
+    //Verificar o tailwind.config e ver que dá pra transformar a classe em função
 
-        // Vairáveis de tipografia 
-        // py-[12px] = px-2xs = 12px (variável colocada no config, dá o valor nas sugestões clicando Ctrl Espaço. Por conta do comentário)
-        // rounded-[8px] = rounded-md
-        <>
-            <button
-                className={`
-            minhas-estilizacoes
-            bg-primary
-            bg-opacity-50
-            rounded-md
-            px-6
-            py-2
-            text-white
-            text-xs
-            ${className}`}>
-                {children}
-            </button>
-            {/* Outros exemplos importantes */}
-            {/* <div className="bg-[#2D5BFF] rounded-t-[10px]">Borda superior arredondada com 10 pixels</div>
-            <div className="bg-[#2D5BFF] rounded-r-[10px]">Borda direita arredondada com 10 pixels</div>
-            <div className="bg-[#2D5BFF] rounded-b-[10px]">Borda inferior arredondada com 10 pixels</div>
-            <div className="bg-[#2D5BFF] rounded-l-[10px]">Borda esquerda arredondada com 10 pixels</div>
-            <div className="bg-[#2D5BFF] rounded-br-[10px] m-5 px-4 py-2">Borda inferior direita arredondada com 10 pixels</div> */}
-        </>
-    )
+    // Vairáveis de tipografia 
+    // py-[12px] = px-2xs = 12px (variável colocada no config, dá o valor nas sugestões clicando Ctrl Espaço. Por conta do comentário)
+    // rounded-[8px] = rounded-md
+
+
+    // <button
+    //     className={`
+    // minhas-estilizacoes
+    // bg-primary
+    // text-white
+    // text-xs
+    // bg-opacity-50 
+    // rounded-md 
+    // px-6 
+    // py-2
+    // ${className}`}
+    //     disabled={disabled}
+    //     {...rest}>
+    //     {children}
+    // </button>
+
+    /* Outros exemplos importantes */
+    /* <div className="bg-[#2D5BFF] rounded-t-[10px]">Borda superior arredondada com 10 pixels</div>
+        <div className="bg-[#2D5BFF] rounded-r-[10px]">Borda direita arredondada com 10 pixels</div>
+        <div className="bg-[#2D5BFF] rounded-b-[10px]">Borda inferior arredondada com 10 pixels</div>
+        <div className="bg-[#2D5BFF] rounded-l-[10px]">Borda esquerda arredondada com 10 pixels</div>
+        <div className="bg-[#2D5BFF] rounded-br-[10px] m-5 px-4 py-2">Borda inferior direita arredondada com 10 pixels</div> */
 }
 
 export default Button
