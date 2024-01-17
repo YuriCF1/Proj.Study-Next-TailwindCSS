8f9896025dff17d3769017991e81200ba40c3359
Yuri Cruz <yuricruzf@gmail.com>
2024-01-16 21:59:21 -0300

    Disabled button

--- alfabit/components/button/button.tsx
+++ alfabit/components/button/button.tsx
@@ -1,23 +1,43 @@
 import React from "react"
 
 export type ButtonProps = {
-    children: React.ReactNode
+    children: React.ReactNode;
+    variant?: 'primary' | 'secondary' | 'tertiary'
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 // const button = (props: ButtonProps) => {
 // Colocando o className para que algúem possa injetar qualquer classe
 // ...rest = resto das propriedades
-const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
-    const generalStyles = 'rounded-md px-6 py-2 font-xl'
-    const Btn = (classes: string) => {
-        return <button
-            className={`${generalStyles} ${classes} ${className}`}
-            disabled={disabled}
-            {...rest}>
-            {children}
-        </button>
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+    // const generalStyles = 'rounded-md px-6 py-2 font-xl'
+
+    switch (variant) {
+        case 'primary':
+            return disabled ? "bg-disabled text-disabled" : "bg-primary text-white"
+        case 'secondary':
+            return disabled ? "bg-disabled text-disabled" : "bg-quaternary text-primary"
+        case 'tertiary':
+        // return disabled ? "bg-disabled text-disabled" : "bg-tertiary text-primary"
+        default:
+            return disabled ? 'text-disabled' : 'text-primary'
     }
-    return Btn(disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white')
+}
+
+const Button = ({ variant = 'primary', children, className, disabled, ...rest }: ButtonProps) => {
+
+
+    // const Btn = (classes: string) => {
+    return <button
+        // className={`${generalStyles} ${classes} ${className}`}
+        className={`rounded-md px-6 py-2 font-xl ${getVariant(variant, disabled)} ${className}`}
+        disabled={disabled}
+        {...rest}>
+        {children}
+    </button>
+    // }
+    // return Btn(disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white')
+
+
     // <button>{props.children}</button>
 
     // MANEIRAS DE DECLARAR AS CLASSES
