f564cbe1cabcce26a5c135b88cccc620e6eb4664
Yuri Cruz <yuricruzf@gmail.com>
2024-01-17 22:21:21 -0300

    Setting exportations of variables

--- alfabit/components/Link/Link.stories.ts
+++ alfabit/components/Link/Link.stories.ts
@@ -2,10 +2,23 @@ import Link, { LinkProps } from './Link';
 import { Meta, StoryObj } from "@storybook/react"
 
 const meta: Meta<LinkProps> = {
-    title: "Molecules/Link",
-    tags: ['autodocs'],
+    title: "Molecules/Link", //Sendo sendo associado a subcategoria  de Moléculas
+    // tags: ['autodocs'],
     component: Link,
-    argTypes: {}
+    argTypes: {
+        children: {
+            type: 'string'
+        },
+        href: {
+            type: 'string'
+        },
+        disabled: {
+            type: 'boolean'
+        },
+        className: {
+            type: 'string'
+        }
+    }
 }
 
 export default meta;
@@ -14,5 +27,20 @@ export const Primary: StoryObj<LinkProps> = {
     args: {
         children: "Link",
         href: '/'
+    }
+}
+
+export const Disabled: StoryObj<LinkProps> = {
+    args: {
+        children: "Link",
+        href: "/",
+        disabled: true
+    }
+}
+export const Violet: StoryObj<LinkProps> = {
+    args: {
+        children: "Link",
+        href: "/",
+        className: "theme-violet"
     }
 }
