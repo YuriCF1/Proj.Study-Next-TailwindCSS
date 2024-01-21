52e860f0334a915d5a953f704c588ca82427e4e2
Yuri Cruz <yuricruzf@gmail.com>
2024-01-21 14:38:43 -0300

    New variables of themes

--- alfabit/components/Input/Input.stories.ts
+++ alfabit/components/Input/Input.stories.ts
@@ -6,7 +6,23 @@ import Input, { InputProps } from './Input';
 const meta: Meta<InputProps> = {
     title: 'Molecules/Input',
     component: Input,
-    argTypes: {}
+    argTypes: {
+        label: {
+            type: "string",
+        },
+        disabled: {
+            type: "boolean"
+        },
+        multiline: {
+            type: "boolean"
+        },
+        placeholder: {
+            type: 'string'
+        },
+        type: {
+            type: 'string'
+        }
+    }
 }
 
 export default meta;
@@ -17,9 +33,54 @@ export const Primary: StoryObj<InputProps> = {
     }
 }
 
-export const Disabled: StoryObj<InputProps> = {
+export const PrimaryDisabled: StoryObj<InputProps> = {
     args: {
         value: 'Input',
         disabled: true
+    }
+}
+
+export const Multiline: StoryObj<InputProps> = {
+    args: {
+        value: 'Input',
+        multiline: true
+    }
+}
+
+export const MultilineDisabled: StoryObj<InputProps> = {
+    args: {
+        value: 'Input',
+        multiline: true,
+        disabled: true
+    }
+}
+
+export const PrimaryLabel: StoryObj<InputProps> = {
+    args: {
+        value: 'Input',
+        label: 'Nome'
+    }
+}
+export const PrimaryLabelDisabled: StoryObj<InputProps> = {
+    args: {
+        value: 'Input',
+        label: 'Nome',
+        disabled: true
+    }
+}
+
+export const MultilineLabel: StoryObj<InputProps> = {
+    args: {
+        value: 'Input',
+        label: 'Nome',
+        multiline: true
+    }
+}
+export const MultilineLabelDisabled: StoryObj<InputProps> = {
+    args: {
+        value: 'Input',
+        label: 'Nome',
+        disabled: true,
+        multiline: true
     }
 }
